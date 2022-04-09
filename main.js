6c1e2ebfe8cc137b703b57833a95f50b22f62c2c
SourishSingh <93921293+SourishSingh@users.noreply.github.com>
2022-04-09 14:21:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+canvas= document.getElementById("o");
+ctx=canvas.context("2d");
+ctx.beginPath()
+ctx.strokeStyle = "red";
+ctx.lineWidth = 4;
+ctx.rect(150, 143, 430, 200);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(250, 210, 40, 0, 2 * Math.PI)
+ctx.stroke();
